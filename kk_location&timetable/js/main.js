71048c4e41e7a79676e0ad2ae99212c0d5ca522c
lorrie <lorrrieli@yahoo.com>
2022-08-20 18:48:51 +1000

    kk - location and timetable

---
+++ kk_location&timetable/js/main.js
@@ -0,0 +1,53 @@
+
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  $('.minus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+    
+    if (value > 1) {
+      value = value - 1;
+    } else {
+      value = 0;
+    }
+    
+    $input.val(value);
+        
+  });
+
+  $('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+
+    if (value < 100) {
+    value = value + 1;
+    } else {
+      value =100;
+    }
+
+    $input.val(value);
+  });
